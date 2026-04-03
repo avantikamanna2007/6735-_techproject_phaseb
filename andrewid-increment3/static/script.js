5935da6169013cbdc0dd62941026f3f5828ebd78
avantikamanna2007 <avantikamanna12@gmail.com>
2026-04-02 21:40:04 -0400

    Add files via upload

--- andrewid-increment3/static/script.js
+++ andrewid-increment3/static/script.js
@@ -1,42 +1,42 @@
 
-var x = 5;
-var y = 7;
-var z = x + y;
-console.log(z);
+// var x = 5;
+// var y = 7;
+// var z = x + y;
+// console.log(z);
 
-var A = "Hello ";
-var B = "world!";
-var C = A + B;
-console.log(C);
+// var A = "Hello ";
+// var B = "world!";
+// var C = A + B;
+// console.log(C);
 
-function sumnPrint(x1, x2) {
-    console.log(x1 + x2);
-}
+// function sumnPrint(x1, x2) {
+//     console.log(x1 + x2);
+// }
 
-sumnPrint(x, y);
-sumnPrint(A, B);
+// sumnPrint(x, y);
+// sumnPrint(A, B);
 
-if (C.length > z) {
-    console.log(C);
-} else if (C.length < z) {
-    console.log(z);
-} else {
-    console.log("good job!");
-}
+// if (C.length > z) {
+//     console.log(C);
+// } else if (C.length < z) {
+//     console.log(z);
+// } else {
+//     console.log("good job!");
+// }
 
-var L1 = ["Watermelon","Pineapple","Pear","Banana"];
-var L2 = ["Apple","Banana","Kiwi","Orange"];
+// var L1 = ["Watermelon","Pineapple","Pear","Banana"];
+// var L2 = ["Apple","Banana","Kiwi","Orange"];
 
-function findTheBanana(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === "Banana") {
-            alert("Banana found!");
-        }
-    }
-}
+// function findTheBanana(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] === "Banana") {
+//             alert("Banana");
+//         }
+//     }
+// }
 
-findTheBanana(L1);
-findTheBanana(L2);
+// findTheBanana(L1);
+// findTheBanana(L2);
 
 
 // PART 3
@@ -68,5 +68,3 @@ function addYear() {
     document.getElementById("copyYear").innerHTML = "© " + year + " MonoMuse. All rights reserved.";
 }
 
-// call the function
-addYear();
